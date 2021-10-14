ff2e654f7ad1d1981d064977f9281faede542b67
harsh101010 <Dilse3456*>
2021-10-14 15:45:08 +0100

    no strategy data test, just to see if everything is fitting to-gather

--- src/test/java/com/acme/mytrader/strategy/TradingStrategyTest.java
+++ src/test/java/com/acme/mytrader/strategy/TradingStrategyTest.java
@@ -1,12 +1,57 @@
 package com.acme.mytrader.strategy;
 
+import com.acme.mytrader.execution.ExecutionService;
+import com.acme.mytrader.price.PriceListener;
+import com.acme.mytrader.price.PriceListenerImpl;
+import com.acme.mytrader.price.PriceSource;
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.listeners.InvocationListener;
+
+import static org.mockito.ArgumentMatchers.anyDouble;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
 public class TradingStrategyTest {
+
+    // Dumb implementation for priceSource Interface
+    InvocationListener priceSourceInvokationListener = methodInvocationReport -> {
+        if(methodInvocationReport.threwException()){
+            methodInvocationReport.getThrowable().printStackTrace();
+        }else{
+            if(methodInvocationReport.getInvocation().toString().contains("addPriceListener")) {
+                // it is assumed that priceListener is listening to priceSource object in real implementation
+                System.out.println("price listener is listening to the priceSource");
+            }else if(methodInvocationReport.getInvocation().toString().contains("removePriceListener")) {
+                System.out.println("price listener is no longer listening to the priceSource");
+            }
+        }
+    };
+
     @Test
     public void testNothing() {
         TradingStrategy tradingStrategy = new TradingStrategy(null,null);
         tradingStrategy.listenTransaction("IBM", 55.0d);
     }
 
+    @Test
+    public void shouldDoNothingWhenStrategyCriteriaIsEmpty(){
+
+        ExecutionService executionService = Mockito.mock(ExecutionService.class);
+        // given the trading strategy is empty
+        TradingStrategy strategy = TradingStrategyFactory.createEmptyStrategy(executionService);
+        TradingStrategy tradingStrategySpy = spy(strategy);
+        PriceListener priceListener = new PriceListenerImpl(tradingStrategySpy);
+        // given priceListener is listening to priceSource
+        PriceSource priceSourceMock = Mockito.mock(PriceSource.class, Mockito.withSettings().invocationListeners(priceSourceInvokationListener));
+        priceSourceMock.addPriceListener(priceListener);
+
+        // when a price event is received and strategy criteria is empty
+        priceListener.priceUpdate("IBM", 55);
+
+        // then no trade is executed
+        verify(tradingStrategySpy).listenTransaction("IBM", 55);
+        verify(executionService, never()).buy(anyString(), anyDouble(), anyInt());
+    }
 }
